8d188b9f2e21c2a2619d5b38c16c1224ec34f607
Adnan Mouhandiz <amouhand@c4r9p1.1337.ma>
2023-12-29 05:02:58 +0100

    Wrong still

--- N-queens.c
+++ N-queens.c
@@ -7,64 +7,82 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
-// int	isValid(int posY, int posX, char **queens)
-// {
-// 	int columns = 0;
-// 	int rows = 0;
-// 	for (int i = 0 ; queens[i] ; i++)
-// 		for(int j = 0; queens[i][j] ; j++)
-// 		{
-
-// 		}
-
-// }
-
-// char*** solveNQueens(int n, int* returnSize, int** returnColumnSizes)
-// {
-// 	char **array;
-// 	int columns = n;
-// 	int rows = n;
-// 	array = malloc(sizeof(char *) * (n + 1));
-
-// 	for (int y = 0 ; y < columns ; y++)
-// 		for (int x = 0 ; x < rows ; x++)
-// 		{
-// 			if(isValid(y, x, array))
-// 			{
-// 				array[y][x] = 'Q';
-// 				for (int tmp = x + 1 ; tmp < rows ; tmp++)
-// 					array[y][x] = '.';
-// 				break;
-// 			}
-// 			else
-// 				array[y][x] = '.';
-// 		}
-// 		return (&array);
-// }
-
-int main()
+#include <stdlib.h>
+int	isValid(int posY, int posX, int **queens, int n)
 {
-	int n = 4;
-	char **array = malloc(sizeof(char *) * (n + 1));
-	for(int i = 0 ; i < n ;i++)
+	int columns = 0;
+	int rows = 0;
+	for (int i = 0 ; i < n, queens[i]; i++)
 	{
-		strcpy();
+		for(int j = 0; i < n, queens[i][j] ; j++)
+		{
+			if (queens[i][j] == 1 && ((posY - i == posX - j) ||
+							(posX == j) || (posY == i) ||
+							(posX - 1 == j && posY - 1 == i) ||
+							(posX + 1 == j && posY + 1 == i)))
+				return 0;
+		}
 	}
+	return 1;
+}
+
+char*** solveNQueens(int n, int* returnSize, int** returnColumnSizes)
+{
+	char ***array;
 	int columns = n;
 	int rows = n;
-	array = malloc(sizeof(char *) * (n + 1));
+	int x = 0;
+	int y = 0;
 
-	for (int y = 0 ; y < columns ; y++)
-		for (int x = 0 ; x < rows ; x++)
+	returnSize = malloc(sizeof(int));
+	array = malloc(sizeof(char **) * (n + 1));
+	array[n] = NULL;
+	returnColumnSizes = malloc(sizeof(int *) * n);
+
+	for (int i = 0; i < n; i++)
+	{
+		returnColumnSizes[i] = malloc(sizeof(int) * n);
+		array[i] = malloc(sizeof(char) * (n + 1));
+		for (int j = 0; j < n; j++)
 		{
-			if(isValid(y, x, array))
+      		array[i][j] = malloc(sizeof(char) * (n + 1));
+		}
+	}
+	for (; y < columns ; y++)
+		for (x = 0; x < rows ; x++)
+		{
+			if(isValid(y, x, returnColumnSizes, n))
 			{
-				array[y][x] = 'Q';
-				for (int tmp = x + 1 ; tmp < rows ; tmp++)
-					array[y][x] = '.';
+				array[y][x][0] = 'Q';
+				returnColumnSizes[y][x] = 1;
+				for (int tmp = x ; tmp < rows - 1; tmp++)
+				{
+					array[y][tmp + 1][0] = '.';
+					returnColumnSizes[y][tmp + 1] = 0;
+				}
 				break;
 			}
 			else
-				array[y][x] = '.';
+			{
+				array[y][x][0] = '.';
+				returnColumnSizes[y][x] = 0;
+			}
 		}
+		*returnSize = n;
+		return (array);
+}
+
+int main()
+{
+	int **returnColumnSizes;
+	int returnSizes;
+	int n = 8;
+	char ***solved = solveNQueens(n, &returnSizes, returnColumnSizes);
+    for (int i = 0; i < n; i++)
+	{
+        for (int j = 0; j < n; j++) {
+            printf("%c", solved[i][j][0]);
+        }
+        printf("\n");
+	}
 }
