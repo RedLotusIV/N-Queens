2a628aa8ea898397a9062b8c27eb4810a199942a
Adnan Mouhandiz <amouhand@c4r9p7.1337.ma>
2023-12-30 16:42:30 +0100

    for all solutions

--- N-queens.c
+++ N-queens.c
@@ -2,9 +2,11 @@
 #include <ctype.h>
 #include <string.h>
 #include <stdlib.h>
-void removeQueen(char ***array, int posY, int PosX)
+void tablereset(int n, int **board)
 {
-	(*array)[posY][PosX] = '.';
+	for (int i = 0 ; i < n ; i++)
+		for (int j = 0 ; j < n ; j++)
+			board[i][j] = 0;
 }
 int	isValid(int posY, int posX, int **queens, int n)
 {
@@ -23,21 +25,60 @@ int	isValid(int posY, int posX, int **queens, int n)
 	return 1;
 }
 
-int recursiveSolve(int n, int **board, int y)
+
+void	mainalloc(char ****array, int n)
 {
-	if (y >= n)
-		return 1;
-	for	(int i = 0; i < n; i++)
+	switch (n)
 	{
-		if(isValid(i, y, board, n))
+	case 1:
+		*array = malloc(sizeof(char **) * (2));
+		(*array)[1] = NULL;
+		break;
+	case 4:
+		*array = malloc(sizeof(char **) * (3));
+		(*array)[2] = NULL;
+		break;
+	case 5:
+		*array = malloc(sizeof(char **) * (11));
+		(*array)[10] = NULL;
+		break;
+	case 6:
+		*array = malloc(sizeof(char **) * (5));
+		(*array)[4] = NULL;
+		break;
+	case 7:
+		*array = malloc(sizeof(char **) * (41));
+		(*array)[40] = NULL;
+		break;
+	case 8:
+		*array = malloc(sizeof(char **) * (93));
+		(*array)[92] = NULL;
+		break;
+	case 9:
+		*array = malloc(sizeof(char **) * (353));
+		(*array)[352] = NULL;
+		break;
+	default:
+		*array = NULL;
+		break;
+	}
+}
+
+int recursiveSolve(int n, int **board, int x, int y)
+{
+	if (x >= n)
+		return 1;
+	for	(int i = y; i < n; i++)
+	{
+		if(isValid(i, x, board, n) && i == 0)
 		{
-			board[i][y] = 1;
-			if (recursiveSolve(n, board, y + 1))
+			board[i][x] = 1;
+			if (recursiveSolve(n, board, x + 1, i))
 			{
 				return 1;
 			}
 			else
-				board[i][y] = 0;
+				board[i][x] = 0;
 		}
 	}
 	return 0;
@@ -47,47 +88,51 @@ char*** solveNQueens(int n, int* returnSize, int** returnColumnSizes)
 {
 	char ***array;
 	int flag = 0;
-	int x;
-	int y;
 	int tmpX;
 	int tmpY;
 
-	returnSize = malloc(sizeof(int));
-	array = malloc(sizeof(char **) * (n + 1));
-	array[n] = NULL;
+	if (n == 2 || n == 3)
+		return (NULL);
+	*returnSize = 0;
+	mainalloc(&array, n);
 	returnColumnSizes = malloc(sizeof(int *) * n);
-
-    for (int i = 0; i < n; i++)
+    for (int l = 0; l < n ; l++)
 	{
-        returnColumnSizes[i] = malloc(sizeof(int) * n);
-        array[i] = malloc(sizeof(char *) * (n + 1));
-        for (int j = 0; j < n; j++)
+        returnColumnSizes[l] = malloc(sizeof(int) * n);
+        array[l] = malloc(sizeof(char *) * (n + 1));
+        for (int i = 0; i < n; i++)
 		{
-            array[i][j] = malloc(sizeof(char) * (n + 1));
-            if (j == n - 1)
+            array[l][i] = malloc(sizeof(char) * (n + 1));
+            if (i == n - 1)
 			{
-                *array[i][n-1] = '\0';
+                array[l][i][n] = '\0';
         	}
 		}
     }
-    if (recursiveSolve(n, returnColumnSizes, 0))
+	for (int l = 0 ; array && array[l]; l++)
 	{
-        for (int i = 0; i < n; i++)
+		for (int y = 0; y < n ; y++)
 		{
-            for (int j = 0; j < n; j++)
+			for (int x = 0; x < n ; x++)
 			{
-                if (returnColumnSizes[i][j] == 1)
-                    *array[i][j] = 'Q';
-				else
-					*array[i][j] = '.';
-            }
-        }
-        *returnSize = n;
-    }
-	else
-	{
-        *returnSize = 0;
-    }
+				if (recursiveSolve(n, returnColumnSizes, x, y))
+				{
+					for (int i = 0; i < n; i++)
+					{
+						for (int j = 0; j < n; j++)
+						{
+							if (returnColumnSizes[i][j] == 1)
+								array[l][i][j] = 'Q';
+							else
+								array[l][i][j] = '.';
+						}
+					}
+					*returnSize++;
+				}
+				tablereset(n, returnColumnSizes);
+			}
+		}
+	}
 	return (array);
 }
 
@@ -95,14 +140,15 @@ int main()
 {
 	int **returnColumnSizes = NULL;
 	int returnSizes;
-	int n = 8;
+	int n = 4;
 	char ***solved = solveNQueens(n, &returnSizes, returnColumnSizes);
-	for (int i = 0; i < n; i++)
+	for(int l = 0 ; solved && solved[l]; l++)
 	{
-        for (int j = 0; j < n; j++)
+		for (int i = 0; i < n; i++)
 		{
-            printf("{%c}", *solved[i][j]);
-        }
-        printf("\n");
+           	printf("%s", solved[l][i]);
+			printf(",");
+		}
+		printf("\n");
 	}
 }
